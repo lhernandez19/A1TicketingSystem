05a3bb076acb1ee40373761a9baead31880793c9
lhernandez19 <diegotapia1987@gmail.com>
2021-01-31 21:20:30 -0600

    new2

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ namespace A1TicketingSystem
             // Exit program.
             else
             {
-                Console.WriteLine("Closing program...");
+                Console.WriteLine("Closing program....");
             }
         }
 
